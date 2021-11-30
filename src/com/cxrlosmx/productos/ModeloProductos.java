257e6974dda94c88e8b39a966b6be67ae3073757
CxrlosMX <LuisCRendon131@gmail.com>
2021-11-29 18:48:04 -0600

    Metodo para agregar datos a la BBDD

--- src/com/cxrlosmx/productos/ModeloProductos.java
+++ src/com/cxrlosmx/productos/ModeloProductos.java
@@ -1,6 +1,8 @@
 package com.cxrlosmx.productos;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -44,11 +46,26 @@ public class ModeloProductos {
 	}
 	public void agregarElProducto(Productos producto) {
 		//1.-Obtener la conexion
+		Connection miConexion=null;
+		PreparedStatement miStatement=null;
+		
+		try {
+			//Establecemos la conexiòn
+			miConexion=origenDatos.getConnection();
+			
 		
 		//2.-Crear la instruccion SQL para insertar datos
-		
+		String instruccionSQL="INSERT INTO PRODUCTO (nombre, precio) VALUES (?,?)";
+		miStatement=miConexion.prepareStatement(instruccionSQL);
 		//3.-Establecer los parametros para el producto
+		miStatement.setString(1,producto.getNombre());
+		miStatement.setInt(2, producto.getPrecio());
 		
 		//4.-Ejecutar la instruccion SQL
+		miStatement.execute();
+		
+		} catch (Exception e) {
+			
+		}
 	}
 }
